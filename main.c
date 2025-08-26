533ebbed7825da9dda717c7663ea8d0a7a1408cd
iamfurkann <esadfurkanduman@gmail.com>
2025-08-26 19:47:39 +0300

    last

--- main.c
+++ main.c
@@ -1,19 +1,23 @@
-
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int	main(void)
 {
 	char	*s;
-	int		fd;
+	int fd = open("test.txt", O_RDONLY);
+	int i = 0;
 
-	fd = open("test.txt", O_RDONLY);
-	while ((s = get_next_line(fd)) != NULL)
+	while (i < 3 && (s = get_next_line(fd)))
 	{
-		printf("satir: %s", s);
+		printf("%s", s);
 		free(s);
+		i++;
 	}
+	
 	close(fd);
+	get_next_line(-1);
 	return (0);
 }
+
